fe0f294de979279bc71b1c9cc973c0299cbe23b5
Rafael <rafamalaga86@gmail.com>
2025-07-29 16:40:16 -0500

    Fixed problem with Nextjs consuming too many DB connections

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-const isDev = process.env.ENV === 'development';
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more: https://pris.ly/d/help/next-js-best-practices
 
-type Global = typeof global & { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = (global as Global).prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (isDev) {
-  (global as Global).prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
