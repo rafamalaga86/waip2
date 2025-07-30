e73f681c6e23fc40693b5dc5fb01dc179cc3cb17
Rafael <rafamalaga86@gmail.com>
2025-07-30 15:45:20 -0500

    Fixed confetti

--- src/app/(main)/PlayingNowMasonry.tsx
+++ src/app/(main)/PlayingNowMasonry.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { Box, CardActions, Link, NoSsr } from '@mui/material';
 import { useState } from 'react';
 import Confetti from 'react-confetti';
@@ -15,16 +16,11 @@ export function PlayingNowMasonry({
   initialItems: any[];
   authUser: UserVisible | null;
 }) {
-  const celebrationDurationSeconds = 10;
   const [items, setGames] = useState(initialItems);
   const [celebrating, setCelebrating] = useState(false);
 
   function removeGame(gameId: number) {
-    const newGames = items.filter(element => {
-      return gameId !== element.id;
-    });
-
-    setGames(newGames);
+    setGames(prev => prev.filter(el => el.id !== gameId));
   }
 
   function showCelebration() {
@@ -33,28 +29,34 @@ export function PlayingNowMasonry({
 
   return (
     <NoSsr defer>
-      {celebrating && <Confetti recycle={false} numberOfPieces={3000} />}
+      {celebrating && <Confetti recycle={false} numberOfPieces={3000} height={4000} />}
+
       <CardsMasonry>
-        {items.map((item: any) => {
+        {items.map((item: any, index: number) => {
           const [fontSize, extraClasses] = titleAdjustment(item.name, 1.2);
+
           return (
             <GameCard
               key={item.id}
               game={item}
+              index={index}
               imgElement={
                 <IGDBImage stringId={item.igdb_cover_id} description={item.name + ' cover'} />
               }
             >
-              <Box
-                sx={{ fontSize: fontSize }}
-                className={'text-align-center title-font ' + extraClasses}
-              >
+              <Box sx={{ fontSize }} className={`text-align-center title-font ${extraClasses}`}>
                 <Link className="color-white" href={`/games/${item.id}?idgbId=${item.igdb_id}`}>
                   {item.name}
                 </Link>
               </Box>
+
               <CardActions
-                sx={{ display: 'flex', justifyContent: 'center', marginTop: 1, paddingBottom: 0 }}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  marginTop: 1,
+                  paddingBottom: 0,
+                }}
               >
                 <GameCardActions
                   game={item}
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,19 +1,33 @@
 import { Card, CardContent } from '@mui/material';
+import { motion } from 'framer-motion';
 import { ReactNode } from 'react';
 
 export function GameCard({
   game,
   imgElement,
+  index,
   children,
 }: {
   game: { id: number; name: string };
   imgElement: ReactNode;
   children: ReactNode;
+  index: number;
 }) {
   return (
-    <Card className="game-card" key={game.id} component="div">
-      {imgElement}
-      <CardContent>{children}</CardContent>
-    </Card>
+    <motion.div
+      layout="position"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.35,
+        layout: { duration: 0.35, ease: 'easeOut' },
+        delay: index * 0.05,
+      }}
+    >
+      <Card className="game-card" component="div">
+        {imgElement}
+        <CardContent>{children}</CardContent>
+      </Card>
+    </motion.div>
   );
 }
