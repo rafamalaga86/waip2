035c48c1a6cf956f5c0f903f6530555ade369422
Rafael <rafamalaga86@gmail.com>
2025-08-04 10:31:43 -0500

    Updated env.dist

--- src/lib/apiRequests.ts
+++ src/lib/apiRequests.ts
@@ -1,7 +1,7 @@
 import { Prisma } from '@prisma/client';
 
 const PLAYED_URL = `/playeds`;
-const BASE_URL = process.env.NEXT_PUBLIC_PRIVATE_API_URL;
+const BASE_URL = process.env.API_VERSIONED_PATH;
 
 export async function deletePlayedRequest(id: number) {
   const url = BASE_URL + PLAYED_URL + '/' + id;
